803f3a46b8f0856760f14c2024d1d97d60c7f302
dhairya7120 <dhairya.sharma@digiqt.com>
2021-12-21 15:22:37 +0530

    register integration

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,12 @@ import ScrollToTop from "./scrollToTop";
 import LoaderWrapper from "./loaderWrapper";
 import Loader from "../views/common/loader";
 import {loadingPageRouters} from "../views/landing/LandingRouter"
+import { loginAndRegisterPageRouters } from "../views/Login&Register/Login&RegisteRouter";
 // import AppHeader from "../views/common/Header/Header";
 
 const LandingPage = React.lazy(() => import("../views/landing"));
 const DashBoardPage = React.lazy(() => import("../views/dashboard"));
-
+const SignupPage = React.lazy(()=>import("../views/Login&Register/index"))
 
 function WaitingComponent(Component) {
   return (props) => (
@@ -42,8 +43,18 @@ export default function AppRouter({ ...props }) {
                     path="/welcome"
                     component={WaitingComponent(LandingPage)}
                   />
+               
                   {/* <Route exact path="/" component={WaitingComponent(LandingPage)} /> */}
-
+                  {loginAndRegisterPageRouters.map((value,index)=>{
+                    return(
+                      <Route 
+                        exact={value.exact}
+                        path={value.path} 
+                        component={WaitingComponent(value.component)} 
+                        key={index}
+                      />
+                    )
+                  })}
                   {loadingPageRouters.map((value,index)=>{
                     return(
                       <Route 
