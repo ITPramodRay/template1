7f4d5ab2b54b99afb895110dbfef19c5d25877b2
prakash-zoogla <90841771+prakash-zoogla@users.noreply.github.com>
2021-12-22 22:10:31 +0530

    Invest dashboard

--- src/views/dashboard/Invest/Invest.js
+++ src/views/dashboard/Invest/Invest.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import Addslider from './Addslider/Addslider';
+import { Container,Row,Col } from 'reactstrap';
+
+import InvestTitle from './InvestHeader/InvestTitle';
+import InvestCard from './InvestHeader/InvestCard';
+import InvestLeftPanel from './InvestHeader/InvestLeftPanel';
 
 
 export default function Invest({ ...props }) {
   
   return (
     <>
-       <Addslider />
+       <InvestTitle />
+       
+       <div>
+        <Container>
+          <Row>
+            <Col md={9}>
+                <InvestCard />
+            </Col>
+            <Col md={3}>
+              <InvestLeftPanel />
+              
+            </Col>
+          </Row>
+        </Container>
+       </div>
     </>
   );
 }
